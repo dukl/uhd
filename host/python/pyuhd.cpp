166a1416e21a4f31cd895a664c74d27e46f29c32
Brent Stapleton <brent.stapleton@ettus.com>
2018-04-03 17:44:38 -0700

    python: adding GIL release
    Copying from MPM, but not actually adding it anywhere yet.

--- host/python/pyuhd.cpp
+++ host/python/pyuhd.cpp
@@ -104,6 +104,32 @@ struct iterable_to_python_list
     }
 };
 
+//! RAII-style GIL release method
+//
+// To release the GIL using this method, simply instantiate this class in the
+// scope that needs to release the GIL.
+//
+// Note that using this class assumes that threads have already been
+// initialized. See also https://docs.python.org/3.5/c-api/init.html for more
+// documentation on Python initialization and threads.
+class scoped_gil_release
+{
+public:
+    inline scoped_gil_release()
+    {
+        _thread_state = PyEval_SaveThread();
+    }
+
+    inline ~scoped_gil_release()
+    {
+        PyEval_RestoreThread(_thread_state);
+        _thread_state = nullptr;
+    }
+
+private:
+    PyThreadState* _thread_state;
+};
+
 // We need this hack because import_array() returns NULL
 // for newer Python versions.
 // This function is also necessary because it ensures access to the C API
